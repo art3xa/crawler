3a265060022cfee6e0196c628680dfe5317b7a7d
Евгений <evgeniisergeev2002@list.ru>
2022-10-05 14:10:13 +0500

    nhti

--- src/crawler.py
+++ src/crawler.py
@@ -1,15 +1,19 @@
 import asyncio
-import re
 import urllib.robotparser
 from dataclasses import dataclass
 from typing import Optional, List
+from lxml import html
 
 import aiohttp
 from yarl import URL
 
 MAX_DEPTH = 10
 PARSED_URLS = set()
-VISITED_HOSTS = {}
+rp = urllib.robotparser.RobotFileParser()
+rp.set_url('https://habr.com/robots.txt')
+rp.read()
+
+VISITED_HOSTS = {'habr.com': rp}
 
 
 @dataclass
@@ -18,10 +22,16 @@ class Task:
 
 
 class MyNormalWrapperForHTMLParser:
-    link_regex = re.compile('<a\\s*href=[\'|"](.*?)[\'"].*?>')
-
     def parse(self, data):
-        return [a for a in self.link_regex.findall(data)]
+        tree = html.fromstring(data)
+        linkList = tree.xpath('//a')
+        result = []
+        for link in linkList:
+            url = str(link.get('href'))
+            if '.' in url and 'http' in url[0:5]:
+                result.append(url)
+
+        return result
 
 
 @dataclass
@@ -37,41 +47,46 @@ class FetchTask(Task):
         res = []
         parser = MyNormalWrapperForHTMLParser()
         for link in parser.parse(data):
-            try:
-                new_url = URL(link)
-            except BaseException:
-                print(link)
+            new_url = URL(link)
 
-            if new_url.host is None and new_url.path.startswith('/') \
-                    and str(new_url).encode().decode() == str(new_url):
+            if new_url.host is None and new_url.path.startswith('/'):
                 new_url = URL.build(
                     scheme=self.url.scheme,
                     host=self.url.host,
                     path=new_url.path,
                     query_string=new_url.query_string
-                )
-                robots_url = str(URL.build(
+                )  # ЭТО АБСОЛЮТНЫЙ ЮРЛ /aboba.php -> vk.com/aboba.php
+
+            if len(VISITED_HOSTS) > 0 and list(VISITED_HOSTS.keys())[0] not in new_url.host:
+                continue
+
+            if self.url.host not in VISITED_HOSTS:
+                robots_url = URL.build(
                     scheme=self.url.scheme,
                     host=self.url.host,
                     path="/robots.txt"
-                ))
+                )  # ЭТО ЧЁ
 
-                if self.url.host not in VISITED_HOSTS:
-                    rp = urllib.robotparser.RobotFileParser()
-                    rp.set_url(robots_url)
-                    rp.read()
-                    VISITED_HOSTS[self.url.host] = rp
+                rp = urllib.robotparser.RobotFileParser()
+                rp.set_url(str(robots_url))
+                rp.read()
+                VISITED_HOSTS[self.url.host] = rp  # ВСЕГДА ОДИН ХОСТ
 
-                if new_url in PARSED_URLS \
-                        or not VISITED_HOSTS[self.url.host] \
-                        .can_fetch("*", str(new_url)):
-                    continue
-                PARSED_URLS.add(new_url)
-                res.append(FetchTask(
-                    tid=self.tid,
-                    url=new_url,
-                    depth=self.depth + 1
-                ))
+            if new_url in PARSED_URLS \
+                    or not VISITED_HOSTS[self.url.host].can_fetch("*", str(new_url)):
+                continue  # ПРОВЕРКА: ЮРЛ УЖЕ БЫЛ ИЛИ РОБОТС.ТХТ ЗАПРЕЩАЕТ НАМ ХОДИТЬ ТУДАЭ
+
+                # print(VISITED_HOSTS)
+
+            PARSED_URLS.add(new_url)  # ДОБАВЛЯЕМ НОВУЮ ССЫЛОЧКУ
+            res.append(FetchTask(
+                tid=self.tid,
+                url=new_url,
+                depth=self.depth + 1
+            ))
+        for i in [str(re) for re in res]:
+            if 'twitter.com' in i:
+                print("pizda")
         return res
 
     async def perform(self, pool):
@@ -81,9 +96,12 @@ class FetchTask(Task):
                           'Chrome/86.0.4240.198 Safari/537.36 '
                           'OPR/72.0.3815.465 (Edition Yx GX)',
         }
+
         async with aiohttp.ClientSession() as session:
+            if 'twitter.com' in str(self.url):
+                print('xyi')
             async with session.get(self.url, headers=user_agent) as resp:
-                # print(self.url, resp.status)
+                print(self.url, resp.status)
                 data = await resp.text()
                 res: List[FetchTask] = \
                     await asyncio.get_running_loop().run_in_executor(
