914aa8d0d7cf9b043c5e709a7dd259f2c706e051
Евгений <evgeniisergeev2002@list.ru>
2022-11-13 21:11:25 +0500

    pep 8

--- src/crawler.py
+++ src/crawler.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 import urllib.robotparser
 from dataclasses import dataclass
 from typing import Optional, List
@@ -25,7 +26,7 @@ class WrapperForHTMLParser:
         result = []
         for link in link_list:
             url = str(link.get('href'))
-            if '.' in url and 'http' in url[0:5]:
+            if ('.' in url and 'http' in url[0:5]) or url[0] == "/":  # парсим абсол и относ ссылки ВСЕ НАХУЙ
                 result.append(url)
 
         return result
@@ -36,31 +37,38 @@ class FetchTask(Task):
     tid: int
     url: URL
     depth: int
+    Downloads: str = r"C:\Users\user\Desktop\Donwloads\\"
 
-    def parser(self, data: str) -> List['FetchTask']:
+    def parser(self, cur_page_link, data: str) -> List['FetchTask']:
         if self.depth + 1 > MAX_DEPTH:
             return []
         res = []
-        for link in WrapperForHTMLParser().parse_links(data):
+        clear_no_filter_urls = []
+        links_on_cur_page = WrapperForHTMLParser().parse_links(data)
+        for link in links_on_cur_page:
             new_url = URL(link)
+            # clear_no_filter_urls.append(new_url)
 
             if new_url.host is None and new_url.path.startswith('/'):
+                clear_no_filter_urls.append(new_url)  # относ ссылки
                 new_url = URL.build(scheme=self.url.scheme, host=self.url.host,
                                     path=new_url.path,
                                     query_string=new_url.query_string)
 
+
             if len(VISITED_HOSTS) > 0 and list(VISITED_HOSTS.keys())[0] not in new_url.host:
                 continue
-
+            clear_no_filter_urls.append(new_url)
             if new_url.host not in VISITED_HOSTS:
                 robots_url = URL.build(scheme=new_url.scheme,
                                        host=new_url.host, path="/robots.txt")
                 rp = urllib.robotparser.RobotFileParser()
                 rp.set_url(str(robots_url))
                 rp.read()
-                VISITED_HOSTS[new_url.host] = rp
+                VISITED_HOSTS[self.url.host] = rp
 
-            if new_url in PARSED_URLS or not VISITED_HOSTS[self.url.host].can_fetch("*", str(new_url)):
+            if new_url in PARSED_URLS or (
+                    self.url.host in VISITED_HOSTS and not VISITED_HOSTS[self.url.host].can_fetch("*", str(new_url))):
                 continue
 
             PARSED_URLS.add(new_url)
@@ -69,8 +77,39 @@ class FetchTask(Task):
                 url=new_url,
                 depth=self.depth + 1
             ))
-
+        self.save_page(self.url, data, clear_no_filter_urls)
         return res
+
+    def save_page(self, url, data, parsed_urls):
+        t = data
+        print(parsed_urls)
+        parsed_urls = list(map(str, parsed_urls))
+        for i in range(len(parsed_urls)):
+            path = ""
+            if "http" in parsed_urls[i][0:4] and '.' in parsed_urls[i]:
+                path = self.Downloads + URL(parsed_urls[i]).host + URL(parsed_urls[i]).path
+                path = path.replace("/", "\\")
+                if path[-1] == '\\':
+                    path = path[0:-1] + ".html"
+            elif "/" == parsed_urls[i][0]:
+                path = self.Downloads + URL(url).host + URL(parsed_urls[i]).path
+                path = path.replace("/", "\\")
+                if path[-1] == '\\':
+                    path = path[0:-1] + ".html"
+            path = path.replace("//", "/")
+            if "&" not in path and "=" not in path:
+                t = t.replace(f'"{parsed_urls[i]}"', f'"{path}"')
+
+        path = self.Downloads + url.host + url.path
+        path = path.replace("/", "\\")
+        if path[-1] == '\\':
+            path = path[0:-1] + ".html"
+
+        if not os.path.exists(os.path.dirname(path)):
+            os.makedirs(os.path.dirname(path))
+
+        with open(path, "w") as f:
+            f.write(t)
 
     async def perform(self, pool):
         user_agent = {
@@ -85,11 +124,12 @@ class FetchTask(Task):
                 print(self.url, resp.status)
                 if resp.content_type == 'text/html':
                     data = await resp.text()
-                    res: List[FetchTask] = \
+                    # self.save_page(self.url, data)
+                    list_tasks: List[FetchTask] = \
                         await asyncio.get_running_loop().run_in_executor(
-                            None, self.parser, data
+                            None, self.parser, self.url, data
                         )
-                    for task in res:
+                    for task in list_tasks:
                         await pool.queue.put(task)
 
 
